9457c767340b13267564bf41b208828fba850e10
Emma Davis <emmadavi35@gmail.com>
2020-08-05 16:07:21 -0400

    Added error and root mean square error calcualtions to all numerical methods

--- HeunsMethod_2ndOrder.m
+++ HeunsMethod_2ndOrder.m
@@ -41,3 +41,18 @@ end
 plot(t, y);
 xlabel('t [s]');
 ylabel('y');
+
+% %ERROR - Comment out all lines below if error is not needed. Note: the
+% %anayltical solution must be solved by hand first before calculating the
+% %error
+% 
+% %Change this, analytical solution, make sure that there are . in front of *
+% %and / because yA and t are arrays
+% yA = 7.*exp(-t).*cos(sqrt(2).*t) + 11./sqrt(2).*exp(-t).*sin(sqrt(2).*t) + sin(t) - cos(t);
+% 
+% %Calculate error
+% error = abs(yA - y);
+% %Calculate root mean square error
+% rmse = sqrt(sum(error, 'all')^2/length(error));
+% %Print out root mean square error
+% rmse
